41c3090d33637987ef83903056b49fbb92e4cf6d
abb00717 <abb00717@gmail.com>
2026-07-02 13:16:32 +0800

    Add README.md and Clean up junk files

--- windows/phases.h
+++
@@ -1,8 +0,0 @@
-void phase_1(char *input);
-void phase_2(char *input);
-void phase_3(char *input);
-void phase_4(char *input);
-void phase_5(char *input);
-void phase_6(char *input);
-void phase_defused();
-void secret_phase(void);
